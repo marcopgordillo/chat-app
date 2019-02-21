af947a29a41a97f22206a100fd518bb85d1eedc1
marcopgordillo <6589245+marcopgordillo@users.noreply.github.com>
2019-02-21 15:58:31 -0500

    Implementado enviar y recibir mensajes en el chat

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Client } from '@stomp/stompjs';
 import * as SockJS from 'sockjs-client';
+import { Mensaje } from './models/mensaje';
 
 @Component({
   selector: 'app-chat',
@@ -11,11 +12,14 @@ export class ChatComponent implements OnInit {
 
   private client: Client;
   conectado = false;
+  mensaje: Mensaje;
+  mensajes: Mensaje[] = [];
 
   constructor() { }
 
   ngOnInit() {
     this.client = new Client();
+    this.mensaje = new Mensaje();
     this.client.webSocketFactory = () => {
       return new SockJS('http://localhost:8080/chat-websocket');
     };
@@ -23,6 +27,13 @@ export class ChatComponent implements OnInit {
     this.client.onConnect = (frame) => {
       console.log('Conectados: ' + this.client.connected + ' : ' + frame);
       this.conectado = true;
+
+      this.client.subscribe('/chat/mensaje', evento => {
+        const mensaje: Mensaje = JSON.parse(evento.body) as Mensaje;
+        mensaje.fecha = new Date(mensaje.fecha);
+        this.mensajes.push(mensaje);
+        console.log(mensaje);
+      });
     };
 
     this.client.onDisconnect = (frame) => {
@@ -36,8 +47,12 @@ export class ChatComponent implements OnInit {
   }
 
   desconectar() {
-
     this.client.deactivate();
+  }
+
+  enviarMensaje(): void {
+    this.client.publish({destination: '/app/mensaje', body: JSON.stringify(this.mensaje)});
+    this.mensaje.texto = '';
   }
 
 }
