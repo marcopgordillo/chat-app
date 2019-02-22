faec83669d9ed1a385d15ef70c15f7b14f1261c0
marcopgordillo <6589245+marcopgordillo@users.noreply.github.com>
2019-02-22 10:22:21 -0500

    Notificar nuevo usuario conectado al chat

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -34,6 +34,9 @@ export class ChatComponent implements OnInit {
         this.mensajes.push(mensaje);
         console.log(mensaje);
       });
+
+      this.mensaje.tipo = 'NUEVO_USUARIO';
+      this.client.publish({destination: '/app/mensaje', body: JSON.stringify(this.mensaje)});
     };
 
     this.client.onDisconnect = (frame) => {
@@ -51,6 +54,7 @@ export class ChatComponent implements OnInit {
   }
 
   enviarMensaje(): void {
+    this.mensaje.tipo = 'MENSAJE';
     this.client.publish({destination: '/app/mensaje', body: JSON.stringify(this.mensaje)});
     this.mensaje.texto = '';
   }
--- src/app/chat/models/mensaje.ts
+++ src/app/chat/models/mensaje.ts
@@ -1,4 +1,6 @@
 export class Mensaje {
   texto = '';
   fecha: Date;
+  username: string;
+  tipo: string;
 }
